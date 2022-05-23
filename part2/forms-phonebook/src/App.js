46f76cd05882c289ae4051acecba065112d0fe9b
Joar Christoffersen <jc@fjellsport.no>
2022-05-23 14:44:49 +0200

    Added rest of part2: 2.15-2.20

--- part2/forms-phonebook/src/App.js
+++ part2/forms-phonebook/src/App.js
@@ -1,20 +1,30 @@
 import { useState, useEffect } from 'react';
-import axios from 'axios';
 import Filter from './components/Filter';
 import Persons from './components/Persons';
 import PersonForm from './components/PersonForm';
+import personService from './services/persons';
+import './index.css';
 
 const App = () => {
   const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState('');
   const [number, setNumber] = useState('');
   const [filter, setFilter] = useState('');
+  const [errorMessage, setErrorMessage] = useState(null);
 
   useEffect(() => {
-    axios.get('http://localhost:3001/persons').then((response) => {
+    personService.getAll().then((response) => {
       setPersons(response.data);
     });
   }, []);
+
+  const Notification = ({ message }) => {
+    if (message === null) {
+      return null;
+    }
+
+    return <div className="error">{message}</div>;
+  };
 
   const filteredPersons = persons.filter((person) => person.name.toLowerCase().includes(filter.toLowerCase()));
 
@@ -22,7 +32,7 @@ const App = () => {
     event.preventDefault();
     const newPerson = {
       name: newName,
-      id: persons.length + 1,
+      id: Math.random() * 10000,
       number: number,
     };
     if (persons.find((person) => person.name === newName)) {
@@ -30,18 +40,33 @@ const App = () => {
       return setNewName('');
     } else {
       setPersons(persons.concat(newPerson));
+      setErrorMessage(`${newName} has been added to phonebook`);
+      setTimeout(() => {
+        setErrorMessage(null);
+      }, 5000);
       setNewName('');
       setNumber('');
+    }
+    personService.create(newPerson).then((response) => {});
+  };
+
+  const deletePerson = (id) => {
+    const person = persons.find((person) => person.id === id);
+    if (window.confirm(`Delete ${person.name}?`)) {
+      personService.remove(id).then((response) => {
+        setPersons(persons.filter((person) => person.id !== id));
+      });
     }
   };
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification message={errorMessage} />
       <Filter filter={filter} setFilter={setFilter} />
       <PersonForm addPerson={addPerson} newName={newName} setNewName={setNewName} number={number} setNumber={setNumber} />
       <h2>Names and Numbers</h2>
-      <Persons filteredPersons={filteredPersons} />
+      <Persons filteredPersons={filteredPersons} deletePerson={deletePerson} />
     </div>
   );
 };
--- part2/forms-phonebook/src/components/Persons.js
+++ part2/forms-phonebook/src/components/Persons.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const Persons = ({ filteredPersons }) => {
+const Persons = ({ filteredPersons, deletePerson }) => {
   return (
     <div>
       {filteredPersons.map((person) => (
         <p key={person.id}>
-          {person.name} {person.number}
+          {person.name} {person.number} <button onClick={() => deletePerson(person.id)}>Delete</button>
         </p>
       ))}
     </div>
